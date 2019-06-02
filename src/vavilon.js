dffab137cdac73a249171d070a6c44aee1930d0e
Nikita Karamov <nick@karamoff.ru>
2019-06-02 21:59:01 +0300

    Switch from data-attribute to class name
    It gives us a nice performance boost

--- src/vavilon.js
+++ src/vavilon.js
@@ -1,20 +1,11 @@
 const tag = ['%c[vavilon]', 'font-weight:bold;'];
 
-console.debug(...tag, 'Waiting for the page to load');
+console.debug(...tag, 'Waiting for the page to load...');
 
 window.onload = function () {
     console.debug(...tag, 'Initiating...');
 
-    let dictionaryFile;
-
-    const vavilonDictionaries = document.querySelector('meta[property~="vavilon-dict"][content]');
-    if (vavilonDictionaries) {
-        dictionaryFile = vavilonDictionaries.content;
-    } else {
-        dictionaryFile = 'vavilon.json';
-    }
-    console.debug(...tag, 'Dictionary file:', dictionaryFile);
-
-    let elements = document.querySelectorAll('[data-vavilon]');
+    let elements = document.getElementsByClassName('vavilon');
+    console.debug(...tag, `${elements.length} vavilon-enabled elements`);
     console.debug(elements);
 };
