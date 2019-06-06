f7628187e4ace045d39d8080f717d5f256108334
Nikita Karamov <nick@karamoff.ru>
2019-06-07 00:31:05 +0300

    Save language to cookie and get language from cookie

--- src/vavilon.js
+++ src/vavilon.js
@@ -1,11 +1,40 @@
 const vavilon = {
-    curLang: null, // TODO: determine language
+    curLang: null,
     els: [],
     dictCount: 0,
     dicts: {}
 };
 
+function getLangCookie () {
+    console.debug('Getting language from cookie...');
+    const parts = ('; ' + document.cookie).split('; vavilon-lang=');
+    if (parts.length === 2) {
+        console.debug('Cookie found.');
+        return parts[1].split(';')[0];
+    } else {
+        console.debug('Cookie not found.');
+        return null;
+    }
+}
+
+function setLangCookie (lang) {
+    const date = new Date();
+    date.setTime(date.getTime() + (315360000000));
+    const expires = `; expires=${date.toUTCString()}`;
+    document.cookie = `vavilon-lang=${lang || ''}${expires}; path=/`;
+}
+
+function determineLanguage () {
+    const cookieLang = getLangCookie();
+    vavilon.curLang = cookieLang || navigator.language || navigator.userLanguage;
+    console.debug('Language set to ' + vavilon.curLang);
+    if (!cookieLang) {
+        setLangCookie(vavilon.curLang);
+    }
+}
+
 function loadOneDictionary (url, language) {
+    console.debug('Loading dictionary for ' + language);
     // eslint-disable-next-line no-undef
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
@@ -13,6 +42,7 @@ function loadOneDictionary (url, language) {
             vavilon.dicts[language] = JSON.parse(xhr.responseText);
 
             if (Object.keys(vavilon.dicts).length === vavilon.dictCount) {
+                console.debug('All dictionaries have been loaded.');
                 replaceData();
             }
         }
@@ -31,7 +61,9 @@ function loadDictionaries () {
 }
 
 function replaceData () {
-    if (vavilon.curLang !== null && (vavilon.curLang in vavilon.dicts)) {
+    // TODO: ensure normal work with locales
+    if (vavilon.curLang !== null && vavilon.curLang !== document.documentElement.lang && (vavilon.curLang in vavilon.dicts)) {
+        console.debug('Translation necessary and possible.');
         console.debug('Loading elements...');
         vavilon.els = document.getElementsByClassName('vavilon');
         for (const el of vavilon.els) {
@@ -41,4 +73,7 @@ function replaceData () {
 }
 
 console.debug('Waiting for the page to load...');
-window.onload = loadDictionaries;
+window.onload = () => {
+    determineLanguage();
+    loadDictionaries();
+};
