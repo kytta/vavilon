e21df6a134a14962f475931a358dbf483de76a19
Nikita Karamov <nick@karamoff.ru>
2019-07-13 19:03:57 +0300

    style: lint code

--- src/vavilon.ts
+++ src/vavilon.ts
@@ -15,26 +15,26 @@ export class Vavilon {
      *
      * The locale comes from user-set cookie or browser language
      */
-    userLocale: Locale;
+    private readonly userLocale: Locale;
 
     /**
      * The original page locale
      *
      * The locale comes from the `lang` attribute of the `<html>` tag
      */
-    pageLocale: Locale;
+    private readonly pageLocale: Locale;
 
     /**
      * The collection of all vavilon-enabled elements on the page
      */
-    elements: HTMLCollectionOf<HTMLElement>;
+    private elements: HTMLCollectionOf<HTMLElement>;
 
     /**
      * The map of available dictionaries
      *
      * The keys are the {@link Locale} codes, whereas the values are the actual {@link Dictionary}s
      */
-    dictionaries: { [key: string]: Dictionary };
+    private readonly dictionaries: { [key: string]: Dictionary };
 
     /**
      * The dictionary that is used to translate the page
@@ -42,14 +42,14 @@ export class Vavilon {
      * If the {@link userLocale} matches the {@link pageLocale}, the value of this is `null`.
      * However, if the user switches to a new language, and then back to {@link pageLocale}, the value will be {@link pageLocale}.
      */
-    pageDict: Locale;
+    private pageDict: Locale;
 
     /**
      * Indicates whether the {@link pageDict} has been loaded
      */
-    pageDictLoaded: boolean;
+    public pageDictLoaded: boolean;
 
-    constructor() {
+    public constructor() {
         this.userLocale = getUserLocale();
         this.pageLocale = getPageLocale();
 
@@ -62,20 +62,20 @@ export class Vavilon {
     /**
      * Finds all vavilon-enabled elements on the page and stores them inside {@link elements}.
      */
-    findAllElements() {
+    public findAllElements(): void {
         this.elements = document.getElementsByClassName('vavilon') as HTMLCollectionOf<HTMLElement>;
     }
 
     /**
      * Replaces all elements' texts with strings provided in the dictionary
      */
-    replaceAllElements(): void {
+    public replaceAllElements(): void {
         if (this.elements && this.pageDict) {
             if (!this.dictionaries[this.pageLocale]) {
                 this.dictionaries[this.pageLocale] = new Dictionary(null);
             }
 
-            Array.from(this.elements).forEach(el => {
+            Array.from(this.elements).forEach((el): void => {
                 const strId = el.dataset.vavilon;
                 if (this.dictionaries[this.pageDict].hasString(strId)) {
                     if (!this.dictionaries[this.pageLocale].hasString(strId)) {
@@ -92,10 +92,10 @@ export class Vavilon {
      *
      * Note that the dictionaries aren't being loaded, only the URLs are parsed
      */
-    registerDictionaries(): void {
+    public registerDictionaries(): void {
         Array.from(document.scripts)
-            .filter(e => e.dataset.hasOwnProperty('vavilonDict'))
-            .forEach(ds => {
+            .filter((e): boolean => e.dataset.hasOwnProperty('vavilonDict'))
+            .forEach((ds): void => {
                 const dictLocale = ds.dataset.vavilonDict.toLowerCase();
                 this.dictionaries[dictLocale] = new Dictionary(ds.src);
             });
@@ -109,12 +109,12 @@ export class Vavilon {
      *
      * @param primaryCb - an optional callback to execute after the {@link pageDict} has been loaded
      */
-    loadDictionaries(primaryCb?: Function): void {
+    public loadDictionaries(primaryCb?: Function): void {
         Object.keys(this.dictionaries)
-            .forEach(loc => {
+            .forEach((loc): void => {
                 if (loc === this.userLocale || loc.slice(0, 2) === this.userLocale.slice(0, 2) && !this.pageDict) {
                     this.pageDict = loc;
-                    this.dictionaries[loc].load(() => {
+                    this.dictionaries[loc].load((): void => {
                         this.pageDictLoaded = true;
                         primaryCb();
                     })
@@ -131,12 +131,14 @@ export class Vavilon {
      *
      * @returns `true` if the switch was possible and successful, `false` if it was not possible
      */
-    changeLocale(localeString: Locale): boolean {
+    public changeLocale(localeString: Locale): boolean {
         if (this.dictionaries[localeString]) {
             this.pageDict = localeString;
+            setLocaleCookie(this.pageDict);
             return true;
         } else if (this.dictionaries[localeString.slice(0, 2)]) {
             this.pageDict = localeString.slice(0, 2);
+            setLocaleCookie(this.pageDict);
             return true;
         }
 
