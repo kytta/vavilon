ac7a3069a13f2a20f7dba20f8c92f469b3691f78
Nikita Karamov <nick@karamoff.ru>
2019-07-13 14:00:14 +0300

    refactor(dictionaries): move one dictionary loading to Dictionary class

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -5,9 +5,11 @@
  * of which are unique string IDs and the values are the strings, translated
  * into the language of the dictionary.
  */
+import {get} from "./http";
+
 export class Dictionary {
 
-    constructor(url: string, strings?: {[key: string]: string}) {
+    constructor(url: string, strings?: { [key: string]: string }) {
         this.url = url;
 
         if (strings) this.strings = strings;
@@ -15,9 +17,16 @@ export class Dictionary {
     }
 
     url: string;
-    strings: {[key: string]: string};
+    strings: { [key: string]: string };
 
     hasString(id: string): boolean {
         return this.strings.hasOwnProperty(id);
+    }
+
+    load(cb?: Function): void {
+        get(this.url, r=> {
+            this.strings = JSON.parse(r);
+            if (cb) cb();
+        })
     }
 }
