17db75e988ceacb3bb064c510c8aeeaf52d29c2a
Nikita Karamov <nick@karamoff.dev>
2020-10-01 09:10:30 +0200

    Try to fix connecting to Browserstack
    See https://github.com/webdriverio/webdriverio/issues/3263 for more info

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,20 +8,6 @@ exports.config = {
   // on a remote machine).
   runner: 'local',
 
-  // =====================
-  // Server Configurations
-  // =====================
-  // Host address of the running Selenium server. This information is usually obsolete as
-  // WebdriverIO automatically connects to localhost. Also, if you are using one of the
-  // supported cloud services like Sauce Labs, Browserstack, Testing Bot or LambdaTest you don't
-  // need to define host and port information because WebdriverIO can figure that out
-  // according to your user and key information. However, if you are using a private Selenium
-  // backend you should define the host address, port, and path here.
-  //
-  hostname: 'hub-cloud.browserstack.com',
-  port: 80,
-  path: '/wd/hub',
-
   // =================
   // Service Providers
   // =================
