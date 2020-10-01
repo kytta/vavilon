48eba3e7307dc89659a29c4b8a5313e32ebe77c8
Nikita Karamov <nick@karamoff.dev>
2020-10-01 09:33:57 +0200

    Remove cookie test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -5,6 +5,5 @@ describe('vavilon.js test page', function () {
     await browser.url('http://localhost:8099');
 
     expect(browser).toHaveTitleContaining('vavilon.js');
-    expect(browser.getCookies('vavilon-locale')).toHaveLength(1);
   });
 });
