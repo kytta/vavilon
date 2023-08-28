b200e003e4617a7da2ebcd0b138187312aa7e255
Nikita Karamov <me@kytta.dev>
2023-08-28 13:58:30 +0200

    Use modern Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from "vite";
-import * as path from "node:path";
+import path from "path";
 
 export default defineConfig({
-  esbuild: {
-    minify: true,
-  },
   build: {
-    sourcemap: true,
-    target: "esnext",
-    minify: "terser",
     lib: {
       entry: path.resolve("./src/index.ts"),
       name: "Vavilon",
       formats: ["es", "umd", "iife"],
-      // TODO: remove this legacy naming in v2
-      fileName: (format) => `vavilon.${format}.js`,
     },
+    minify: "terser",
+    sourcemap: true,
   },
 });
