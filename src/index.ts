4391baedf1de0fbee0a0d30adb4beda6d48d10f4
Nikita Karamov <me@kytta.dev>
2023-08-28 11:13:22 +0200

    Bump TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { Vavilon } from "./vavilon";
 import type { Locale } from "./types";
 
+// This is needed to be able to work with older IE and to be able to assign a new property to Window
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+declare global {
+  interface Window {
+    /**
+     * Changes the page language
+     *
+     * The execution of this method will change the {@link Vavilon.pageLocale}
+     * of the vavilon instance, save the selected locale to cookie and replace
+     * the text of all the vavilon-enabled elements on the page.
+     *
+     * @param localeString - the locale to switch to
+     */
+    setLang(localeString: Locale): void;
+  }
+}
+
 /**
  * Core vavilon object instance
  *
@@ -40,7 +57,7 @@ window.onload = function onload(): void {
  */
 window.setLang = function setLang(localeString: Locale): void {
   const changeSuccessful: boolean = vavilon.setLocale(
-    localeString.toLowerCase()
+    localeString.toLowerCase(),
   );
 
   if (changeSuccessful) {
